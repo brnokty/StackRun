d42694f1ec34f18a2263efdf3cf43e5ad4256175
BrnOkty2 <baran.oktay@mortys.games>
2022-10-07 00:50:19 +0300

    Stack Cut Mechanic, Camera And Chibi Idle Anim

---
+++ Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    [HideInInspector] public Animator _animator;
+
+    private bool flip;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+
+    private void SetTrigger(string parameter)
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName(parameter))
+            return;
+
+        _animator.SetTrigger(parameter);
+    }
+
+    private void SetBool(string parameter, bool value)
+    {
+        _animator.SetBool(parameter, value);
+    }
+
+    private void SetFloat(string parameter, float value)
+    {
+        _animator.SetFloat(parameter, value);
+    }
+
+
+    //Run
+    public void SwitchToRunAnimation(bool value)
+    {
+        SetBool("Run", value);
+    }
+
+    
+    
+    //Dance
+    public void SwitchToDanceAnimation(bool value)
+    {
+        SetBool("Dance", value);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CharacterAnimationController _animationController;
+    [SerializeField] private Transform _chibiTransform;
+
+
+    public void GoNextStack(Transform stackPosition)
+    {
+        
+    }
+}
