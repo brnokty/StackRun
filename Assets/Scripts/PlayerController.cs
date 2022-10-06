2445cb7dea39357ee66bcd5430854d2fee622577
BrnOkty2 <baran.oktay@mortys.games>
2022-10-07 00:59:55 +0300

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterAnimationController _animationController;
     [SerializeField] private Transform _chibiTransform;
 
+    private Tween moveTween;
+    private Tween lookAtTween;
 
     public void GoNextStack(Transform stackPosition)
     {
-        
+        moveTween?.Kill();
+        lookAtTween?.Kill();
+
+        _animationController.SwitchToRunAnimation(true);
+
+        var pos = stackPosition.position;
+        pos.y = 0;
+        lookAtTween = _chibiTransform.DOLookAt(pos, 0.5f);
+        moveTween = transform.DOMove(pos, 1f).OnComplete(() =>
+        {
+            _animationController.SwitchToRunAnimation(false);
+            _chibiTransform.DORotate(Vector3.zero, 0.1f);
+        });
     }
 }
